46cf560d692511a0af9b63dfc32c47102f0b7492
RobinXSI <xsirobin@gmail.com>
2014-10-13 11:12:40 +0200

    Initial Commit

---
+++ iBeaconTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iBeaconTest
+//
+//  Created by Robin on 13.10.14.
+//  Copyright (c) 2014 HSR. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
