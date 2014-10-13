c4ec23969af2f7ba4db44eef3bc40162f769831d
RobinXSI <xsirobin@gmail.com>
2014-10-13 14:30:42 +0200

    Did everything

--- iBeaconTest/AppDelegate.swift
+++ iBeaconTest/AppDelegate.swift
@@ -7,15 +7,41 @@
 //
 
 import UIKit
+import CoreLocation
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, CLLocationManagerDelegate{
 
     var window: UIWindow?
+    var locationManager: CLLocationManager?
+    
+    var lastProximity: CLProximity?
+    
+    
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let uuidString = "B9407F30-F5F8-466E-AFF9-25556B57FE6D"
+        let beaconIdentifier = "iBeaconMoules.us"
+        let beaconUUID:NSUUID = NSUUID(UUIDString: uuidString)
+        let beaconRegion:CLBeaconRegion = CLBeaconRegion(proximityUUID: beaconUUID, identifier: beaconIdentifier)
+        
+        locationManager = CLLocationManager()
+        if(locationManager!.respondsToSelector("requestAlwaysAuthorization")){
+            locationManager!.requestAlwaysAuthorization()
+        }
+        locationManager!.delegate = self
+        locationManager!.pausesLocationUpdatesAutomatically = false
+        locationManager!.startMonitoringForRegion(beaconRegion)
+        locationManager!.startRangingBeaconsInRegion(beaconRegion)
+        locationManager!.startUpdatingLocation()
+
+        if(application.respondsToSelector("registerUserNotificationSettings:")) {
+            application.registerUserNotificationSettings(UIUserNotificationSettings(forTypes: UIUserNotificationType.Alert | UIUserNotificationType.Sound, categories: nil))
+        }
+        
         return true
     }
 
@@ -42,5 +68,66 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
 
+}
+
+extension AppDelegate: CLLocationManagerDelegate {
+    func sendLocalNotificationWithMessage(message: String!) {
+        let notification:UILocalNotification = UILocalNotification()
+        notification.alertBody = message
+        
+        UIApplication.sharedApplication().scheduleLocalNotification(notification)
+    }
+    
+    func locationManager(manager: CLLocationManager!, didRangeBeacons beacons: [AnyObject]!, inRegion region: CLBeaconRegion!) {
+        let viewController:ViewController = window!.rootViewController as ViewController
+        viewController.beacons = beacons as [CLBeacon]?
+        viewController.tableView!.reloadData()
+        
+        
+        println("didRangeBeacons")
+        var message : String = ""
+        
+        if(beacons.count > 0){
+            let nearestBeacon:CLBeacon = beacons[0] as CLBeacon
+            
+            if(nearestBeacon.proximity == lastProximity || nearestBeacon.proximity == CLProximity.Unknown) {
+                return
+            }
+            lastProximity = nearestBeacon.proximity
+            
+            switch nearestBeacon.proximity {
+            case CLProximity.Far:
+                message = "You are far away from the beacon"
+            case CLProximity.Near:
+                message = "You are near the beacon"
+            case CLProximity.Immediate:
+                message = "You are in the immediate proximity of the beacon"
+            case CLProximity.Unknown:
+                return
+            }
+        } else {
+            message = "No beacons are nearby"
+        }
+        
+        println(message)
+        sendLocalNotificationWithMessage(message)       
+    }
+    
+    func locationManager(manager: CLLocationManager!, didEnterRegion region: CLRegion!) {
+        manager.startRangingBeaconsInRegion(region as CLBeaconRegion)
+        manager.startUpdatingLocation()
+        
+        println("You entered the region")
+        sendLocalNotificationWithMessage("You entered the region")
+    }
+    
+    func locationManager(manager: CLLocationManager!, didExitRegion region: CLRegion!) {
+        manager.stopRangingBeaconsInRegion(region as CLBeaconRegion)
+        manager.stopUpdatingLocation()
+        
+        println("You exited the region")
+        sendLocalNotificationWithMessage("You exited the region")
+        
+    }
 }
 
---
+++ iBeaconTest/Estimote-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  Estimote-Bridging-Header.h
+//  iBeaconTest
+//
+//  Created by Robin on 13.10.14.
+//  Copyright (c) 2014 HSR. All rights reserved.
+//
+
+#import "ESTBeaconManager.h"
